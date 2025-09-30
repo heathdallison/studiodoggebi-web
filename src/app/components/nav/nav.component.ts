5bf98fbb10b8ea133f5304f6f6ab1db0111825bf
heathdallison <heath.d.allison@gmail.com>
2025-09-29 23:32:52 -0500

    ok. I feel like we have hello world. let's clean this up a bit.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,37 +6,7 @@ import { DomainConfigService, SiteConfig } from '@services/domain-config.service
   selector: 'sd-nav',
   standalone: true,
   imports: [NgIf],
-  template: `
-    <nav class="sd-nav">
-      <a
-        *ngIf="siteConfig.lsIsActive"
-        class="nav-button active"
-        href="https://legendarysisters.com"
-      >
-        Legendary Sisters
-      </a>
-      <span
-        *ngIf="!siteConfig.lsIsActive"
-        class="nav-button"
-      >
-        Legendary Sisters
-      </span>
-
-      <a
-        *ngIf="siteConfig.sdIsActive"
-        class="nav-button active"
-        href="https://studiodoggebi.com"
-      >
-        Studio Doggebi
-      </a>
-      <span
-        *ngIf="!siteConfig.sdIsActive"
-        class="nav-button"
-      >
-        Studio Doggebi
-      </span>
-    </nav>
-  `
+  templateUrl: './nav.component.html'
 })
 export class NavComponent {
   siteConfig: SiteConfig;
