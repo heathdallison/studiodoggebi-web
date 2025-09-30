72fafdf2d027b2e4dfb6c23cd1721cce6919e2ef
heathdallison <heath.d.allison@gmail.com>
2025-09-29 21:36:53 -0500

    styles broken suddenly

--- src/app/components/masthead/masthead.component.ts
+++ src/app/components/masthead/masthead.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { DomainConfigService, SiteConfig } from '@services/domain-config.service';
 
 @Component({
   selector: 'sd-masthead',
   standalone: true,
-  imports: [NgClass],
   template: `
-<header class="sd-masthead">
-  <h1>{{ siteConfig.masthead }}</h1>
-</header>
-`,
-styles: [`
-  .sd-masthead {
-    text-align: center;
-    padding: 20px 0;
-    font-size: 2rem;
-    font-weight: bold;
-  }
-`]
+    <header class="sd-masthead">
+      <h1>{{ siteConfig.masthead }}</h1>
+    </header>
+  `,
+  styles: [`
+    .sd-masthead {
+      text-align: center;
+      padding: 20px 0;
+      font-size: 2rem;
+      font-weight: bold;
+    }
+  `]
 })
 export class MastheadComponent {
   siteConfig: SiteConfig;
