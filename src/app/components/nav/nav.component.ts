7097b0fc02c4cbfe25542d52fcc812bbc4213363
heathdallison <heath.d.allison@gmail.com>
2025-10-01 10:07:20 -0500

    replacing a with routerlink to preserve state

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,29 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
-import { LayoutInfoService } from '../../services/layout-info.service'; // ← use relative path
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'sd-nav',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, RouterLink],
   templateUrl: './nav.component.html'
 })
 export class NavComponent {
-  siteConfig: {
-    masthead: string;
-    lsIsActive: boolean;
-    sdIsActive: boolean;
-  };
-
-  nav: { label: string; url: string; disabled: boolean }[];
-
-  constructor(private layoutInfo: LayoutInfoService) {
-    this.siteConfig = {
-      masthead: this.layoutInfo.masthead,
-      lsIsActive: this.layoutInfo.flags.lsIsActive,
-      sdIsActive: this.layoutInfo.flags.sdIsActive
-    };
-
-    this.nav = this.layoutInfo.nav;
-  }
+  @Input() nav: { label: string; url: string; disabled?: boolean; external?: boolean }[] = [];
 }
