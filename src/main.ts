941605c144f3131892e4fa25cba676023bd95cce
heathdallison <heath.d.allison@gmail.com>
2025-09-30 20:25:02 -0500

    It's so annoying when simple things don't do what they're told.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import domainConfig from './app/config/domain';
+import { AppComponent } from 'app/app';
+import { DOMAIN_CONFIG } from './app/config/domain-config.token';
+
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: DOMAIN_CONFIG,
+      useValue: domainConfig
+    }
+  ]
 });
+
